f613471b3b0049ebb5987579b93ddcf0c0347bf8
Laurent René de Cotret <l.rdc@socivolta.com>
2023-03-16 09:26:13 -0400

    Exposing internal structure of a Series via an Internal module

---
+++ src/Data/Series/Internal.hs
@@ -0,0 +1,23 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Series.Internal
+-- Copyright   :  (c) Laurent P. René de Cotret 2002
+-- License     :  MIT
+-- Maintainer  :  laurent.decotret@outlook.com
+-- Portability :  portable
+--
+-- = WARNING
+--
+-- This module is considered __internal__.
+--
+-- The Package Versioning Policy __does not apply__.
+--
+-- The contents of this module may change __in any way whatsoever__
+-- and __without any warning__ between minor versions of this package.
+--
+-- Authors importing this module are expected to track development
+-- closely.
+
+module Data.Series.Internal ( Series(..) ) where
+
+import Data.Series.Definition( Series(..) )
