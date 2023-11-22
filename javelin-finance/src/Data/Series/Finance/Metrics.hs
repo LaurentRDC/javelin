20a638f908367ddf0b5d920b5f260e6610cb47d3
Laurent René de Cotret <l.rdc@socivolta.com>
2023-11-22 14:31:57 -0500

    Resurrect the javelin-finance package including a single-pass max draw down

--- javelin-finance/src/Data/Series/Finance/Metrics.hs
+++ javelin-finance/src/Data/Series/Finance/Metrics.hs
@@ -1,51 +1,36 @@
+{-# LANGUAGE RecordWildCards #-}
+-- Having strict accumulators is key to keeping a good
+-- memory footprint, given that all metrics need to be computed
+-- in a single pass.
+{-# LANGUAGE StrictData      #-}
+module Data.Series.Finance.Metrics (
+    Metric, compute,
 
-module Data.Series.Finance.Metrics ( 
+    -- * Basic financial metrics
     sharpeRatio,
     sortinoRatio,
-
-    -- * Drawdowns
-    DrawDown, amount, duration, recovery,
-    drawdowns, maxDrawDown,
+    maxDrawDown,
 ) where
 
-import qualified Data.List.NonEmpty  as NE
+import           Control.Foldl       ( Fold(..) )
+import qualified Control.Foldl       as Fold
+import           Data.Series.Generic ( Series )
+import qualified Data.Series         as Boxed
 import           Data.Vector.Generic ( Vector )
-import           Data.Series.Generic ( Series, select, index )
-import qualified Data.Series.Generic as GSeries
-import           GHC.Real            ( infinity )
 
 
 -- $setup
 -- >>> import qualified Data.Series as Series
 
-
--- | \(O(n)\) Calculate the Sharpe ratio of excess returns. 
---
--- >>> let returns = Series.fromList $ zip [(0::Int)..] [ (1.0::Double), 2.0, 3.0, 2.0 ]
--- >>> returns
--- index | values
--- ----- | ------
---     0 |    1.0
---     1 |    2.0
---     2 |    3.0
---     3 |    2.0
--- >>> sharpeRatio returns
--- 2.82842712474619
-sharpeRatio :: ( RealFloat a
-               , Vector v a
-               , Vector v (Int, a, a)
-               ) => Series v k a -> a
--- Note that the standard deviation of returns and standard deviation of excess returns
--- are always equal; the standard deviation is invariant under addition of a constant.
-sharpeRatio returns = GSeries.mean returns 
-                    / GSeries.std returns
+-- | Type synonym for metrics.
+-- Note that through underlying 'Fold' mechanism from "Control.Foldl", a 'Metric' always traverses
+-- a series once. 
+type Metric k a b = Fold (k, a) b
 
 
--- | \(O(n)\) Calculate the Sortino ratio of excess returns.
---
--- We distinguish between two edge cases:
--- 1. If the input series is empty, the result is @NaN@;
--- 2. if the input series is not empty, but there are no negative returns, the result is +infinity.
+-- | Compute a given metric over a series.
+-- Note that through underlying 'Fold' mechanism, a 'Metric' always traverses
+-- a series once. Therefore, it is better to combine multiple metrics before calling 'compute':
 --
 -- >>> let returns = Series.fromList $ zip [(0::Int)..] [ (1.0::Double), -2.0, 3.0, -1.0 ]
 -- >>> returns
@@ -55,68 +40,71 @@ sharpeRatio returns = GSeries.mean returns
 --     1 |   -2.0
 --     2 |    3.0
 --     3 |   -1.0
--- >>> sortinoRatio returns
--- 0.5
-sortinoRatio :: ( RealFloat a
-                , Vector v a
-                , Vector v Int
-                , Vector v (Int, a, a)
-                , Ord k
-                ) => Series v k a -> a
+-- >>> let allMyMetrics = (,,) <$> maxDrawDown <*> sharpeRatio <*> sortinoRatio
+-- >>> returns `compute` allMyMetrics
+-- (-2.0,0.1301889109808239,0.5000000000000001)
+compute :: Vector v a 
+        => Series v k a 
+        -> Metric k a b 
+        -> b
+compute xs metric 
+    = Fold.fold metric $ Boxed.toVector $ toBoxedSeries xs
+{-# INLINE compute #-}
+    
+
+-- Since unboxed vectors are not foldable, unboxed series are also not foldable.
+-- I don't have a great idea on how to define metrics which can be applied to
+-- boxed and unboxed series, so we resort to converting all unboxed series
+-- to boxed series
+toBoxedSeries :: Vector v a => Series v k a -> Boxed.Series k a
+toBoxedSeries = Boxed.convert
+{-# INLINE toBoxedSeries #-}
+
+
+-- | Upgrade a 'Fold' to a 'Metric', by ignoring keys in a 'Series'.
+ignoringKeys :: Fold a b -> Metric k a b
+ignoringKeys = Fold.premap snd
+{-# INLINE ignoringKeys #-}
+
+
+-- | \(O(n)\) Sharpe ratio of excess returns. 
+--
+-- >>> let returns = Series.fromList $ zip [(0::Int)..] [ (1.0::Double), 2.0, 3.0, 2.0 ]
+-- >>> returns
+-- index | values
+-- ----- | ------
+--     0 |    1.0
+--     1 |    2.0
+--     2 |    3.0
+--     3 |    2.0
+-- >>> returns `compute` sharpeRatio
+-- 2.82842712474619
+sharpeRatio :: RealFloat a => Metric k a a
 -- Note that the standard deviation of returns and standard deviation of excess returns
 -- are always equal; the standard deviation is invariant under addition of a constant.
-sortinoRatio returns
-    | GSeries.length returns == 0    = 0/0
-    | GSeries.length negReturns == 0 = fromRational infinity
-    | otherwise = GSeries.mean returns / GSeries.std negReturns
-    where 
-        negReturns = GSeries.filter (<0) returns
-
--- | Representation of a drawdown: a continuous series of non-positive returns.
-data DrawDown v k a 
-    = MkDrawDown { getDrawDown :: Series v k a
-                 , getRecovery :: Series v k a 
-                 }
+sharpeRatio = ignoringKeys $ (/) <$> Fold.mean <*> Fold.std
+{-# INLINE sharpeRatio #-}
 
 
--- | Determine the amount of a drawdown.
-amount :: (Vector v a, Num a) => DrawDown v k a -> a
-amount = GSeries.sum . getDrawDown
-
-
--- | Determine the length of a drawdown.
+-- | \(O(n)\) Sortino ratio of excess returns.
 --
--- In order to minimize constraints on the key type,
--- the duration is returned as a tuple of the starting key and ending key.
-duration :: Ord k => DrawDown v k a -> (k, k)
-duration dd = let ix = GSeries.index $ getDrawDown dd
-               in (minimum ix, maximum ix)
-
-
--- | Determine the length of the recovery from a drawdown.
+-- There are two edge cases. If the input series is empty, or there are no negative returns, the result is @NaN@;
 --
--- In order to minimize constraints on the key type,
--- the duration is returned as a tuple of the starting key and ending key.
-recovery :: Ord k => DrawDown v k a -> (k, k)
-recovery dd = let ix = GSeries.index $ getRecovery dd
-               in (minimum ix, maximum ix)
-
-
--- | Compute a list of drawdowns from a series of returns.
-drawdowns :: (Vector v a, Num a, Ord a, Ord k) 
-          => Series v k a 
-          -> [DrawDown v k a]
-drawdowns xs = go xs mempty
-    where
-        go ys acc
-            | GSeries.null ys = acc
-            | otherwise = let ys' = GSeries.dropWhile (>0) ys
-                              (dd, after) = part (<=0) ys'
-                              toRecover = -1 * GSeries.sum dd
-                              recov = index $ GSeries.takeWhile (<toRecover) $ GSeries.postscanl (+) 0 after
-                           in go after (acc <> [MkDrawDown dd (after `select` recov)])
-
-        part f s = (GSeries.takeWhile f s, GSeries.dropWhile f s)
+-- >>> let returns = Series.fromList $ zip [(0::Int)..] [ (1.0::Double), -2.0, 3.0, -1.0 ]
+-- >>> returns
+-- index | values
+-- ----- | ------
+--     0 |    1.0
+--     1 |   -2.0
+--     2 |    3.0
+--     3 |   -1.0
+-- >>> returns `compute` sortinoRatio
+-- 0.5000000000000001
+sortinoRatio :: RealFloat a => Metric k a a
+-- Note that the standard deviation of returns and standard deviation of excess returns
+-- are always equal; the standard deviation is invariant under addition of a constant.
+sortinoRatio = ignoringKeys $ (/) <$> Fold.mean <*> Fold.prefilter (<0) Fold.std
+{-# INLINE sortinoRatio #-}
 
 
 -- | Returns the most negative drawdown from a series of returns.
@@ -131,10 +119,21 @@ drawdowns xs = go xs mempty
 --     3 |   -3.0
 --     4 |    1.0
 --     5 |   -5.0
--- >>> maxDrawDown returns
--- -6.0
-maxDrawDown :: (Vector v a, Num a, Ord a, Ord k) 
-            => Series v k a 
-            -> a
-maxDrawDown xs = maybe 0 (minimum . fmap amount) 
-               $ NE.nonEmpty (drawdowns xs)
+-- >>> returns `compute` maxDrawDown
+-- -10.0
+maxDrawDown :: (Ord a, Num a) => Metric k a a
+maxDrawDown = ignoringKeys $ Fold advance (MkDrawDownState 0 0 0) cumulativeMaxDraw
+    where
+        advance MkDrawDownState{..} ret 
+            = let newAccumulatedReturn = ret + cumulativeReturn
+                  draw = newAccumulatedReturn - cumulativeMax
+               in MkDrawDownState{ cumulativeMax     = max cumulativeMax newAccumulatedReturn
+                                 , cumulativeReturn  = newAccumulatedReturn
+                                 , cumulativeMaxDraw = min cumulativeMaxDraw draw
+                                 }
+{-# INLINE maxDrawDown #-}
+
+data DrawDownState a = MkDrawDownState { cumulativeMax     :: !a 
+                                       , cumulativeReturn  :: !a
+                                       , cumulativeMaxDraw :: !a
+                                       }
--- javelin-finance/test/Test/Data/Series/Finance/Metrics.hs
+++ javelin-finance/test/Test/Data/Series/Finance/Metrics.hs
@@ -1,10 +1,9 @@
 module Test.Data.Series.Finance.Metrics ( tests ) where
 
-import           Data.Series.Finance.Metrics  ( sharpeRatio, maxDrawDown, sortinoRatio )
-import qualified Data.Series.Unboxed  as Series
-import qualified Data.Vector.Unboxed  as Vector
-
-import           GHC.Real             ( infinity )
+import           Control.Monad        ( guard )
+import           Data.Series.Finance.Metrics  ( compute, sharpeRatio, maxDrawDown, sortinoRatio )
+import qualified Data.Series          as Series
+import qualified Data.Vector          as Vector
 
 import           Hedgehog             ( property, forAll, assert )
 import qualified Hedgehog.Gen         as Gen
@@ -18,10 +17,11 @@ import           Test.Utils           ( approx, assertApproxEqual )
 
 
 tests :: TestTree
-tests = testGroup "Data.Series.Finance.Metrics" [ testSharpeRatio
-                                                , testSortinoRatio
-                                                , testMaxDrawDown
-                                                ]
+tests = testGroup "Data.Series.Finance.Metrics" 
+    [ testSharpeRatio
+    , testSortinoRatio
+    , testMaxDrawDown
+    ]
 
 
 testSharpeRatio :: TestTree
@@ -32,7 +32,7 @@ testSharpeRatio = testGroup "sharpeRatio"
     where
         testBasicCase = testCase "Sharpe ratio of [1,2,3,2]" $ do
             let xs = Series.fromList $ zip [(0::Int)..] [1.0 :: Double, 2.0, 3.0, 2.0]
-            assertApproxEqual mempty 1e-8 (2 * sqrt 2) (sharpeRatio xs)
+            assertApproxEqual mempty 1e-8 (2 * sqrt 2) (xs `compute` sharpeRatio)
         
         testProp = testProperty "sharpeRatio is mean/std" $ property $ do
             ms <- forAll $ Gen.list (Range.linear 0 100) (Gen.double $ Range.linearFrac (-500) 500)
@@ -42,7 +42,7 @@ testSharpeRatio = testGroup "sharpeRatio"
             -- Note that the std implementation of javelin
             -- uses a single-pass std algorithm, which is why
             -- the property below uses Stats.fastStdDev.
-            sharpeRatio xs `approx` (Stats.mean vs / Stats.fastStdDev vs)
+            xs `compute` sharpeRatio `approx` (Stats.mean vs / Stats.fastStdDev vs)
 
 
 testSortinoRatio :: TestTree
@@ -55,42 +55,43 @@ testSortinoRatio = testGroup "sortinoRatio"
     where
         testBasicCase = testCase "Sortino ratio of [1,-2,3,-1]" $ do
             let xs = Series.fromList $ zip [(0::Int)..] [1.0 :: Double, -2.0, 3.0, -1.0]
-            assertApproxEqual mempty 1e-8 (1/2) (sortinoRatio xs)
+            assertApproxEqual mempty 1e-8 (1/2) (xs `compute` sortinoRatio)
         
         testReturnsNanIfEmpty = testCase "Sortino ratio of empty series is NaN" $ do
-            assertEqual mempty True (isNaN $ sortinoRatio (mempty :: Series.Series Int Double))
+            assertEqual mempty True (isNaN $ (mempty :: Series.Series Int Double) `compute` sortinoRatio)
 
-        testNoNegReturns = testCase "Sortino ratio of non-empty series with no negative returns is infinity" $ do
+        testNoNegReturns = testCase "Sortino ratio of non-empty series with no negative returns is NaN" $ do
             let xs = Series.fromList $ zip [(0::Int)..] [1.0 :: Double, 2.0, 3.0, 4.0]
-            assertEqual mempty (fromRational infinity) (sortinoRatio xs)
+            assertEqual mempty True (isNaN $ xs `compute` sortinoRatio)
         
         testProp = testProperty "Sortino is mean(x)/std(x[x<0])" $ property $ do
             ms <- forAll $ Gen.list (Range.linear 0 100) (Gen.double $ Range.linearFrac (-500) 500)
+            -- If there are no negative returns, the result is NaN which isn't comparable.
+            guard $ any (<0) ms
             let xs = Series.fromList $ zip [(0::Int)..] ms
                 vs = Vector.fromList ms
             
             -- Note that the std implementation of javelin
             -- uses a single-pass std algorithm, which is why
             -- the property below uses Stats.fastStdDev.
-            sortinoRatio xs `approx` (Stats.mean vs / Stats.fastStdDev (Vector.filter (<0) vs))
+            xs `compute` sortinoRatio `approx` (Stats.mean vs / Stats.fastStdDev (Vector.filter (<0) vs))
 
 
 testMaxDrawDown :: TestTree
 testMaxDrawDown = testGroup "maxDrawDown" 
-                [ testBasicCase
-                , testMultipleDrawdowns
-                , testNonPositiveMDDs
+                [ testNonPositiveMDDs
+                , runCase [1, -1]             (-1)
+                , runCase [0, 0, 0, 1, -1, 1] (-1)
+                , runCase [1, -1, -1, 1, 1]   (-2)
+                , runCase [1, 1, -1, -2, 0, -1, 2, 2, 1, -1, 0, 0, 0, 0, 0, -4, 0, 0, 2, 2, 1, -2, 10] (-5)
                 ]
     where
-        testBasicCase = testCase "maxDrawDown of simple case" $ do
-            let xs = Series.fromList $ zip [(0::Int)..] [1 :: Int, -1, -1, 0, -1, 1]
-            assertEqual mempty (-3) (maxDrawDown xs)
-
-        testMultipleDrawdowns = testCase "maxDrawDown of two drawdowns" $ do
-            let xs = Series.fromList $ zip [(0::Int)..] [1 :: Int, -1, -1, 0, -1, 1, -10]
-            assertEqual mempty (-10) (maxDrawDown xs)
+        runCase :: [Double] -> Double -> TestTree
+        runCase pnl expectation = testCase mempty $ do
+            let xs = Series.fromList $ zip [0::Int ..] pnl
+            assertEqual mempty expectation (xs `compute` maxDrawDown)
                 
         testNonPositiveMDDs = testProperty "maxDrawDown is always non-positive" $ property $ do
             ms <- forAll $ Gen.list (Range.linear 0 100) (Gen.double $ Range.linearFrac (-500) 500)
             let xs = Series.fromList $ zip [(0::Int)..] ms
-             in assert (maxDrawDown xs <= 0)
+            assert (xs `compute` maxDrawDown <= 0)
