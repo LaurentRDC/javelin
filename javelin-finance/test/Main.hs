48a972ff8248ef98bfb8d6849afb98b2d56d3269
Laurent Rene de Cotret <l.rdc@socivolta.com>
2024-03-07 08:53:23 -0500

    Remove unused package javelin-finance

--- javelin-finance/test/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-
-import qualified Test.Data.Series.Finance.Metrics
-
-import           Test.Tasty ( defaultMain, testGroup )
-
-main :: IO ()
-main = defaultMain $ testGroup "Test suite" [ Test.Data.Series.Finance.Metrics.tests
-                                            ]
